6e02d1afb3d14a3b1e3462d4ea321a208b49bdd6
Biryukova Nadezhda <biryukova.nadezhda@bk.ru>
2022-09-27 22:21:45 +0300

    Add banner and login form

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 import Popup from './Popup';
 import popupObject from './popupObject';
 
-const body = document.querySelector('body');
-const popup = new Popup(popupObject.login).init();
-body.append(popup);
+const buttonLogin = document.querySelector('.header-interaction__item');
+buttonLogin.addEventListener('click', () => {
+  const body = document.querySelector('body');
+  const popup = new Popup(popupObject.login).init();
+  body.prepend(popup);
+});
